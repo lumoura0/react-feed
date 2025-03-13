f92d3e2b10a7e40899df34542e9235f5a4ade8f3
Luis Fernando Moura Goncalves <lfmgoncalves@emsurb.se.gov.br>
2025-03-13 16:47:59 -0300

    Criando propriedades do Componente Post

--- src/App.jsx
+++ src/App.jsx
@@ -19,14 +19,14 @@ const posts = [
       { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
       { type: 'link', content: 'jane.design/doctorcare' },
     ],
-    publishedAt: new Date('2025-02-02 20:00:00'),
+    publishedAt: new Date('2025-03-03 20:00:00'),
   },
   {
     id: 2,
     author: {
-      avatarUrl: 'https://github.com/maykbrito.png',
-      name: 'Mayk Brito',
-      role: 'Educator @Rocketseat'
+      avatarUrl: 'https://github.com/lumoura0.png',
+      name: 'Fernando Moura',
+      role: 'Dev Frontend',
     },
     content: [
       { type: 'paragraph', content: 'Fala galera 👋' },
