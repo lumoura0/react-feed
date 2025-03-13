2a69496d67b694b32cc84a14f24015870ca02767
Luis Fernando Moura Goncalves <lfmgoncalves@emsurb.se.gov.br>
2025-03-13 13:13:54 -0300

    Finalizando Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,20 @@
 import styles from './Sidebar.module.css'
+import { PencilLine } from 'phosphor-react'
 import capa from '../assets/capa.png'
 export function Sidebar() {
     return (
         <aside className={styles.sidebar}>
             <img className={styles.cover} src={capa} alt="Capa" />
+
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/lumoura0.png" />
                 <strong>Luis Fernando</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20} />
                     Editar seu perfil
                 </a>
             </footer>
